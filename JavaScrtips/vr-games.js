e6f6dd50d3e33ede986745d231f30612b82e4bbc
Andres Hernandez <randresho@gmail.com>
2026-03-31 11:03:51 -0600

    WIP videos vr portfolio

--- JavaScrtips/vr-games.js
+++ JavaScrtips/vr-games.js
@@ -1,22 +1,50 @@
 const videos = [
   {
-    title: "Birds",
+    title: "Proyecto Aldea Global - Panacam",
     id: "cgTvly9-z4k",
-    description: "Un vistazo increíble a las cordilleras en invierno.",
+    client: "Panacam",
+    description:
+      "Leveraging advanced hand-tracking integration, this Unity-powered experience allows users to navigate an interactive spatial UI to discover and summon indigenous wildlife. By combining high-fidelity 3D modeling with immersive spatial acoustics and physics-based gesture inputs, the project delivers a compelling educational narrative focused on biodiversity and environmental conservation.",
     thumb: getThumb("cgTvly9-z4k"),
   },
   {
     title: "Nueva Vida",
     id: "giI7peRFbCE",
+    client: "Cooperativa Nueva Vida",
     description: "Un vistazo increíble a las cordilleras en invierno.",
     thumb: getThumb("giI7peRFbCE"),
   },
   {
-    title: "Nueva Vida",
+    title: "Nueva Vida 360° ",
     id: "60DN1SSKt4k",
-    description: "Un vistazo increíble a las cordilleras en invierno.",
+    client: "Cooperativa Nueva Vida",
+    description:
+      "Transforming facility tours into a scalable digital experience, this application blends immersive 360° photography with interactive 3D elements. Users navigate through various departments via an intuitive spatial interface, experiencing smooth, high-definition transitions that mimic a physical walkthrough. This telepresence tool streamlines the onboarding process for new members, providing a comprehensive and engaging overview of the cooperative’s infrastructure and services.",
     thumb: getThumb("60DN1SSKt4k"),
   },
+  {
+    title: "Gyssa Quiz",
+    id: "-zFJaNs5YEE",
+    client: "Gyssa",
+    description: "Un vistazo increíble a las cordilleras en invierno.",
+    thumb: getThumb("-zFJaNs5YEE"),
+  },
+  {
+    title: "Goalkeeper VR",
+    id: "TrNXwqgMYxE",
+    client: "VR Studio",
+    description:
+      "This experiece is a VR sports simulation where the player takes on the role of a soccer goalkeeper in a crowded stadium. Using physics-based hand tracking, the user must react to incoming penalty shots from an AI striker to defend the goal. The experience features a localized spatial UI with regional flags and dynamic visual feedback, such as screen tints to indicate successful goals or saves. The project demonstrates real-time collision detection and immersive environmental storytelling through animated crowds and digital stadium signage.",
+    thumb: getThumb("TrNXwqgMYxE"),
+  },
+
+  /*{
+    title: "Video Title",
+    id: "Video ID",
+    client: "Client",
+    description: "Description",
+    thumb: getThumb("Video ID"),
+  },*/
 ];
 
 let currentIndex = 0;
@@ -29,6 +57,7 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 const videoElem = document.getElementById("main-video");
 const titleElem = document.getElementById("video-title");
+const clientElem = document.getElementById("client");
 const descElem = document.getElementById("video-description");
 const playlistElem = document.getElementById("playlist-items");
 
@@ -77,6 +106,7 @@ function loadVideo(index) {
 function updateText() {
   const v = videos[currentIndex];
   document.getElementById("video-title").innerText = v.title;
+  document.getElementById("client").innerText = v.client;
   document.getElementById("video-description").innerText = v.description;
 }
 
